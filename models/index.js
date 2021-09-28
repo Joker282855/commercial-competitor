e6ee7987bc7039fd2b0b4f84a4c3257a758b8c58
unknown <jtjackson325@gmail.com>
2021-09-27 23:04:51 -0600

    got the associations worked out finally

--- models/index.js
+++ models/index.js
@@ -17,14 +17,14 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: 'product_tag',
+  as: 'product_tagged',
   foreignKey: 'product_id'
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: 'tagged_merch',
+  as: 'product_tagged',
   foreignKey: 'tag_id'
 });
 
